ac6f4c52a52e47f2bb666a25d274d20c8ab65820
Kry1X <bogdankrylosov08@gmail.com>
2024-10-31 13:28:58 +0300

    test_commit

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,10 @@ const start = async () => {
     try {
         await sequelize.authenticate()
         await sequelize.sync()
-        app.listen(PORT, () => console.log(`Server started >>>\nhttp://localhost:${PORT}`))
+        app.listen(PORT, () => console.log(
+            '-----------------------------------------------\n',
+            `Server started...\n >>> http://localhost:${PORT}`
+        ))
     } catch (e) {
         console.log(e)
     }
