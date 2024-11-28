42d10a507626500c12da8e061e293c2c007b88c0
Kry1X <bogdankrylosov08@gmail.com>
2024-11-29 02:10:50 +0300

    Fix bugs, work with friendsController

--- backend/models/models.js
+++ backend/models/models.js
@@ -1,23 +1,29 @@
-const sequelize = require('../db')
-const {DataTypes} = require('sequelize')
+const sequelize = require('../db');
+const { DataTypes } = require('sequelize');
 
-const User = sequelize.define('user', {
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    email: {type: DataTypes.STRING, unique: true},
-    username: {type: DataTypes.STRING, unique: true},
-    password: {type: DataTypes.STRING},
-    role: {type: DataTypes.STRING, defaultValue: "USER"}
-})
+const User = sequelize.define('users', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    email: { type: DataTypes.STRING, unique: true },
+    username: { type: DataTypes.STRING, unique: true },
+    password: { type: DataTypes.STRING },
+    role: { type: DataTypes.STRING, defaultValue: "USER" }
+});
 
 const Friends = sequelize.define('friends', {
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    first_id: {type: DataTypes.INTEGER, allowNull: false, references: {model: 'user', key: 'id'}},
-    second_id: {type: DataTypes.INTEGER, allowNull: false, references: {model: 'user', key: 'id'}}
-})
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    first_id: { type: DataTypes.INTEGER, allowNull: false, references: { model: 'users', key: 'id' }, onDelete: 'CASCADE' },
+    second_id: { type: DataTypes.INTEGER, allowNull: false, references: { model: 'users', key: 'id' }, onDelete: 'CASCADE' }
+    }, {
+        indexes: [
+            {unique: true, fields: ['first_id', 'second_id']},
+            {unique: true, fields: ['second_id', 'first_id']}
+        ]
+    });
 
-User.belongsToMany(User, {through: Friends, foreignKey: 'first_id', otherKey: 'second_id'})
+User.hasMany(Friends, { foreignKey: 'first_id'})
+User.hasMany(Friends, { foreignKey: 'second_id'})
 
 module.exports = {
     User,
     Friends
-}
+};
--- backend/routes/friendsRouter.js
+++ backend/routes/friendsRouter.js
@@ -3,8 +3,9 @@ const router = new Router()
 const friendsController = require('../controllers/friendsController')
 
 router.post('/add_friend', friendsController.addFriend)
-router.get('/get_friend', friendsController.getFriend)
-router.get('/get_friends', friendsController.getFriends)
+router.get('/get_friend/:id', friendsController.getFriend)
+router.post('/get_friends', friendsController.getFriends)
+router.get('/get_all_friends', friendsController.getAllFriends)
 router.delete('/delete_friend', friendsController.deleteFriend)
 
 module.exports = router
