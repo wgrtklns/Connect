ecfe0fa88e7bf94bfa2d733be4cea35cd923cfbe
Kry1X <bogdankrylosov08@gmail.com>
2024-12-06 02:25:10 +0300

    Add musicController, musicRouter

--- backend/controllers/musicController.js
+++ backend/controllers/musicController.js
@@ -2,7 +2,29 @@ const {} = require('../models/models')
 
 
 class MusicController {
+    async uploadMusic(req, res) { // next?
+        
+    }
+
+    async getMusic(req, res) { 
+
+    }
+
+    async deleteMusic(req, res) {
+
+    }
     
+    async addToFavorite(req, res) {
+
+    }
+ 
+    async getFavorites(req, res) {
+
+    } 
+
+    // async getFavSong(req, res) {}
+
+    // async deleteFromFav(req, res) {}
 }
 
 module.exports = new MusicController()
--- backend/routes/musicRouter.js
+++ backend/routes/musicRouter.js
@@ -2,7 +2,12 @@ const Router = require('express')
 const router = new Router()
 const musicController = require('../controllers/musicController')
 
-
-
+router.post('/upload_m', musicController.uploadMusic)
+router.get('/get_m', musicController.getMusic)
+router.delete('/delete_m', musicController.deleteMusic)
+router.post('/add_to_fav', musicController.addToFavorite)
+router.get('/get_fav', musicController.getFavorites)
+// router.get('/get_fav_song', musicController.getFavSong)
+// router.delete('/delete_from_fav', musicController.deleteFromFav)
 
 module.exports = router
